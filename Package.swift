b1bc3671c6bcf72853b27565eda756a1aec643ad
Alexander Weiß <weiss_alex@gmx.net>
2022-11-25 10:51:36 +0100

    feat: Add support for watchOS v6 or greater

--- Package.swift
+++ Package.swift
@@ -6,7 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SemanticVersioningKit",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
