bbc8bee3fba354e16420c59f13ba32e56294ad69
Alexander Weiß <weiss_alex@gmx.net>
2023-04-01 09:55:54 +0200

    chore: Update swift-parsing library to 0.12.0
    chore: swift-format

--- Tests/SemanticVersionComparableTests.swift
+++ Tests/SemanticVersionComparableTests.swift
@@ -1,6 +1,6 @@
 //
 //  SemanticVersionComparableTests.swift
-//  
+//
 //
 //  Created by Alexander Weiß on 28.12.20.
 //
@@ -10,59 +10,64 @@ import XCTest
 @testable import SemanticVersioningKit
 
 final class SemanticVersionComparableTests: XCTestCase {
-    
     func testCoreMajorComparison() throws {
         let left = SemanticVersion(major: 1, minor: 0, patch: 0)
         let right = SemanticVersion(major: 2, minor: 0, patch: 0)
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
     }
-    
+
     func testCoreMinorComparison() throws {
         let left = SemanticVersion(major: 1, minor: 1, patch: 0)
         let right = SemanticVersion(major: 1, minor: 2, patch: 0)
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
     }
-    
+
     func testCorePatchComparison() throws {
         let left = SemanticVersion(major: 1, minor: 0, patch: 1)
         let right = SemanticVersion(major: 1, minor: 0, patch: 2)
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
     }
-    
+
     func testCoreReleaseIdentifiers1() throws {
         let left = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha"])
         let right = SemanticVersion(major: 1, minor: 0, patch: 0)
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
     }
-    
+
     func testCoreReleaseIdentifiers2() throws {
         let left = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha"])
         let right = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "1"])
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
     }
-    
+
     func testCoreReleaseIdentifiers3() throws {
         let left = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "1"])
         let right = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "2"])
-        
-        let versionWithAdditions = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "1"], buildIdentifiers: ["20130313144700"])
+
+        let versionWithAdditions = SemanticVersion(
+            major: 1,
+            minor: 0,
+            patch: 0,
+            preReleaseIdentifiers: ["alpha", "1"],
+            buildIdentifiers: ["20130313144700"]
+        )
         print(versionWithAdditions)
-        
+
         XCTAssertLessThan(left, right)
         XCTAssertGreaterThan(right, left)
         XCTAssertNotEqual(left, right)
@@ -74,6 +79,6 @@ final class SemanticVersionComparableTests: XCTestCase {
         ("testCorePatchComparison", testCorePatchComparison),
         ("testCoreReleaseIdentifiers1", testCoreReleaseIdentifiers1),
         ("testCoreReleaseIdentifiers2", testCoreReleaseIdentifiers2),
-        ("testCoreReleaseIdentifiers3", testCoreReleaseIdentifiers3)
+        ("testCoreReleaseIdentifiers3", testCoreReleaseIdentifiers3),
     ]
 }
--- Tests/SemanticVersionTests.swift
+++ Tests/SemanticVersionTests.swift
@@ -1,6 +1,6 @@
 //
 //  SemanticVersionTests.swift
-//  
+//
 //
 //  Created by Alexander Weiß on 28.12.20.
 //
@@ -10,152 +10,156 @@ import XCTest
 @testable import SemanticVersioningKit
 
 final class SemanticVersionTests: XCTestCase {
-    
     func testCore() throws {
         let semverString = "1.0.0"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
     }
-    
-    
+
     func testPreReleaseIdentifiers1() throws {
         let semverString = "1.0.0-alpha"
-        
+
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 1)
         XCTAssertEqual(version.preReleaseIdentifiers[0], "alpha")
     }
-    
+
     func testPreReleaseIdentifiers2() throws {
         let semverString = "1.0.0-alpha.1"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 2)
         XCTAssertEqual(version.preReleaseIdentifiers[0], "alpha")
         XCTAssertEqual(version.preReleaseIdentifiers[1], "1")
     }
-    
+
     func testPreReleaseIdentifiers3() throws {
         let semverString = "1.0.0-x.7.z.92"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 4)
         XCTAssertEqual(version.preReleaseIdentifiers[0], "x")
         XCTAssertEqual(version.preReleaseIdentifiers[1], "7")
         XCTAssertEqual(version.preReleaseIdentifiers[2], "z")
         XCTAssertEqual(version.preReleaseIdentifiers[3], "92")
     }
-    
+
     func testPreReleaseIdentifiers4() throws {
         let semverString = "1.0.0-x-y-z.-"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 2)
         XCTAssertEqual(version.preReleaseIdentifiers[0], "x-y-z")
         XCTAssertEqual(version.preReleaseIdentifiers[1], "-")
     }
-    
+
     func testBuildIdentifiers1() throws {
         let semverString = "1.0.0+20130313144700"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 1)
         XCTAssertEqual(version.buildIdentifiers[0], "20130313144700")
     }
-    
+
     func testBuildIdentifiers2() throws {
         let semverString = "1.0.0+exp.sha.5114f85"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 3)
         XCTAssertEqual(version.buildIdentifiers[0], "exp")
         XCTAssertEqual(version.buildIdentifiers[1], "sha")
         XCTAssertEqual(version.buildIdentifiers[2], "5114f85")
     }
-    
+
     func testBuildIdentifiers3() throws {
         let semverString = "1.0.0+21AF26D3--117B344092BD"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 0)
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 1)
         XCTAssertEqual(version.buildIdentifiers[0], "21AF26D3--117B344092BD")
     }
-    
+
     func testPreAndBuildIdentifiers() throws {
         let semverString = "1.0.0-alpha+001"
 
         let version = try XCTUnwrap(SemanticVersion(input: semverString))
-        
+
         XCTAssertEqual(version.major, 1)
         XCTAssertEqual(version.minor, 0)
         XCTAssertEqual(version.patch, 0)
-        
+
         XCTAssertEqual(version.preReleaseIdentifiers.count, 1)
         XCTAssertEqual(version.preReleaseIdentifiers[0], "alpha")
-        
+
         XCTAssertEqual(version.buildIdentifiers.count, 1)
         XCTAssertEqual(version.buildIdentifiers[0], "001")
     }
-    
+
     func testStringRepresentation() {
         let coreOnly = SemanticVersion(major: 1, minor: 0, patch: 0)
         let withPreRelease = SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "1"])
         let withBuildIdentifiers = SemanticVersion(major: 1, minor: 0, patch: 0, buildIdentifiers: ["21AF26D3--117B344092BD"])
-        let withPreReleaseAndBuildIdentifiers =  SemanticVersion(major: 1, minor: 0, patch: 0, preReleaseIdentifiers: ["alpha", "1"], buildIdentifiers: ["exp","sha","5114f85"])
-        
+        let withPreReleaseAndBuildIdentifiers = SemanticVersion(
+            major: 1,
+            minor: 0,
+            patch: 0,
+            preReleaseIdentifiers: ["alpha", "1"],
+            buildIdentifiers: ["exp", "sha", "5114f85"]
+        )
+
         XCTAssertEqual("1.0.0", "\(coreOnly)")
         XCTAssertEqual("1.0.0-alpha.1", "\(withPreRelease)")
         XCTAssertEqual("1.0.0+21AF26D3--117B344092BD", "\(withBuildIdentifiers)")
