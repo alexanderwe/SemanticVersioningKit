df2b85c65c3a28efea0cb853acba57041c5c9faa
Alexander Weiß <weiss_alex@gmx.net>
2026-09-10 09:14:41 -0700

    chore(deps): update dependencies

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     ],
     dependencies: [
         // Source code dependencies
-        .package(url: "https://github.com/pointfreeco/swift-parsing", exact: "0.14.1"),
+        .package(url: "https://github.com/pointfreeco/swift-parsing", exact: "0.15.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
